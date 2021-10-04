06ece2f0f733c2143239e0c4d695cf505bd87900
Askolini <asmircc@gmail.com>
2021-10-04 09:35:55 +0200

    Done

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -2,8 +2,8 @@ const spcBtn = document.querySelector("#space");
 const bckSpc = document.querySelector("#backspace");
 const entr = document.querySelector("#enter");
 const oki = document.querySelector("#ok");
-// const cpsLk = document.querySelector("#capslook");
-// const shft = document.querySelector("#shift");
+const cpsLk = document.querySelector("#capslook");
+const shft = document.querySelector("#shift");
 
 function disp(result) {
     form.textarea.value = form.textarea.value + result;
@@ -23,4 +23,30 @@ function disp(result) {
     oki.onclick = (()=>{
         form.textarea.value = alert(form.textarea.value);
     })
+};
+
+var capsCheck = 0;
+var shiftCheck = 0;
+
+cpsLk.addEventListener("click", function() {capsBtn()});
+shft.addEventListener("click", function() {shiftBtn()});
+
+function capsBtn() {
+    if(capsCheck == 0) {
+        capsCheck = 1; 
+        cpsLk.style.backgroundColor = "#00FF00";
+    } else {
+        capsCheck = 0; 
+        cpsLk.style.backgroundColor = "#000000";
+    }
+};
+
+function shiftBtn() {
+    if(shiftCheck == 0) {
+        shiftCheck = 1;
+        shft.style.backgroundColor = "#00FF00";
+    } else {
+        shiftCheck = 0;
+        shft.style.backgroundColor = "#000000"
+    }
 };
